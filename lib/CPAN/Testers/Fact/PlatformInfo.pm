6cf7978a31c236546b538541135416aa64907c77
Barbie <barbie@missbarbell.co.uk>
2012-12-02 14:01:16 +0000

    Import of BARBIE/CPAN-Testers-Fact-PlatformInfo-1.01 from CPAN.
    gitpan-cpan-distribution: CPAN-Testers-Fact-PlatformInfo
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         BARBIE/CPAN-Testers-Fact-PlatformInfo-1.01.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/CPAN/Testers/Fact/PlatformInfo.pm
+++ lib/CPAN/Testers/Fact/PlatformInfo.pm
@@ -1,9 +1,9 @@
-use 5.006;
-use strict;
-use warnings;
 package CPAN::Testers::Fact::PlatformInfo;
 
-our $VERSION = '1.00';
+use strict;
+use warnings;
+
+our $VERSION = '1.01';
 
 # ABSTRACT: platform information on which a CPAN Testers smoker is running.
 
@@ -12,8 +12,8 @@ use Carp ();
 use Metabase::Fact::Hash 0.016;
 our @ISA = qw/Metabase::Fact::Hash/;
 
-#sub required_keys { qw/archname osname osvers oslabel is32bit is64bit osflag codename kernel/ }
 sub required_keys { }   # all keys are optional
+sub optional_keys { qw/archname osname osvers oslabel is32bit is64bit osflag codename kernel/ }
 
 sub content_metadata {
   my ($self) = @_;
@@ -55,7 +55,7 @@ CPAN::Testers::Fact::PlatformInfo - platform information on which a CPAN Testers
 
 =head1 VERSION
 
-version 1.00
+version 1.01
 
 =head1 SYNOPSIS
 
@@ -90,6 +90,10 @@ smoker.
 
 All keys are optional, but here in case some become mandatory.
 
+=item optional_keys
+
+Returns a list of theoptional keys.
+
 =item content_metadata
 
 The metadata values accessor.
@@ -117,12 +121,12 @@ existing test-file that illustrates the bug or desired feature.
 
   Barbie (BARBIE) <barbie@cpan.org>
   Brian McCauley (NOBULL) <nobull67@gmail.com>
-  Colin Newell http://colinnewell.wordpress.com/
+  Colin Newell (NEWELLC) F<http://colinnewell.wordpress.com/>
   Jon 'JJ' Allen (JONALLEN) <jj@jonallen.info>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2011 Birmingham Perl Mongers
+  Copyright (C) 2011-2012 Birmingham Perl Mongers
 
   This module is free software; you can redistribute it and/or
   modify it under the Artistic License 2.0.
